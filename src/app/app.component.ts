ec5c63313de42acf0f884041beda738e8b06a6f2
Ilgner <ilgner.nascimento@plurya.com>
2023-08-08 17:47:11 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'convem-app';
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient){
+    this.form = this.formBuilder.group({
+      msg: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (true) {
+      const formData = this.form.value;
+      this.http.get('http://localhost:3000')
+        .subscribe(
+          (response: any) => {
+            const { results } = response;
+            if(results){
+              Swal.fire({
+                icon: 'success',
+                title: 'Sucesso!',
+                text: 'Dados enviados com sucesso!'
+              });
+            }
+          },
+          (error: any) => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Erro!',
+              text: 'Ocorreu um erro ao enviar os dados.'
+            });
+          }
+        );
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importe esta linha
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule, // Certifique-se de incluir esta linha
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
